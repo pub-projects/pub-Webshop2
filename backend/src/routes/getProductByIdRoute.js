bc81fc5791a5fab7afaf2dc5f9683808026a2009
ChrisJohDev <chris@chrisjohannesson.com>
2021-12-23 14:01:29 +0100

    Initialized database webshop2 and loaded data into collection products from file backend/data-store/initialData-mongodb.json. Functionalities getProductList and getProductById works.

---
+++ backend/src/routes/getProductByIdRoute.js
@@ -0,0 +1,23 @@
+import { ObjectId } from 'mongodb';
+import { getDbConnection } from '../db';
+
+export const getProductById = {
+    path: '/api/products/getProductById/:id',
+    method: 'get',
+    handler: async (req, res) => {
+        try {
+            const db = getDbConnection('webshop2');
+            const { id } = req.params;
+            const o_id = new ObjectId(id); // *** Important *** without this mongodb doesn't find the _id
+
+            const data = await db.collection('products').findOne({ _id: o_id });
+
+            if (!data) return res.sendStatus(401);
+
+            return res.status(200).json(data);
+        } catch (err) {
+            console.log("getProductById:", err);
+            return res.sendStatus(500);
+        }
+    }
+}
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,9 @@
+import { getProductList } from './getProductListRoute';
+import { getProductById } from './getProductByIdRoute';
 import { testRoute } from './testRoute';
 
 export const routes = [
+    getProductList,
+    getProductById,
     testRoute,
 ];
