6d2324cf408dd46014e98ef552b7cf87ace577eb
ChrisJohDev <chris@chrisjohannesson.com>
2021-12-27 12:10:10 +0100

    Functioning home page. Needs styling and final touches.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,14 @@
-
+import { ProductList } from '../components/ProductList';
 
 export const Home = () => {
+
+
+    // const pageData = loadContent();
+
     return (
         <div className="page-container">
 
-
+            <ProductList />
         </div>
     );
 };
