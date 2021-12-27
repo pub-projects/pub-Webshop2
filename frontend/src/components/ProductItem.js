e11d850c1d1042f69976afd7e3bd26e0f50e9b31
ChrisJohDev <chris@chrisjohannesson.com>
2021-12-27 18:19:09 +0100

    Added show more/less functionality to product card.

--- frontend/src/components/ProductItem.js
+++ frontend/src/components/ProductItem.js
@@ -1,10 +1,12 @@
-
+import ShowMoreText from 'react-show-more-text';
 
 export const ProductItem = (item) => {
-    //console.log("ProductItem");
-    //console.log("product:", item.product);
     const product = item.product;
     const urlBase = "/api/images/";
+
+    const executeOnClick = (isExpanded) => {
+        console.log(isExpanded);
+    }
 
     return !product ? (
         <div className="productItemWrapper">
@@ -14,12 +16,43 @@ export const ProductItem = (item) => {
         <div className="card mb-3" style={{ maxWidth: "540px", minWidth: "300px" }}>
             <div className="row g-0">
                 <div className="col-md-4">
-                    <img src={product.imgRef.match('=') ? urlBase + product.imgRef.split('=')[1] : product.imgRef} className="img-fluid rounded-start" alt="..." />
+                    <img src={product.imgRef.match('=') ? urlBase + product.imgRef.split('=')[1] : product.imgRef} className="img-fluid rounded-start" alt={product.name} />
                 </div>
                 <div className="col-md-8">
                     <div className="card-body">
                         <h5 className="card-title">{product.name}</h5>
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <h6 className="card-subtitle mb-2 text-muted" >
+                            <ShowMoreText
+                                /* Default options */
+                                lines={2}
+                                more="Show more"
+                                less="Show less"
+                                className="content-css"
+                                anchorClass="my-anchor-css-class"
+                                onClick={executeOnClick()}
+                                expanded={false}
+                                width={280}
+                                truncatedEndingComponent={"... "}
+                            >
+                                {product.shortDescription}
+                            </ShowMoreText>
+                        </h6>
+                        <p className="card-text">
+                            <ShowMoreText
+                                /* Default options */
+                                lines={2}
+                                more="Show more"
+                                less="Show less"
+                                className="content-css"
+                                anchorClass="my-anchor-css-class"
+                                onClick={executeOnClick()}
+                                expanded={false}
+                                width={280}
+                                truncatedEndingComponent={" ... "}
+                            >
+                                {product.description}
+                            </ShowMoreText>
+                        </p>
                         <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import { ProductItem } from './ProductItem';
 import { ProductConsumer } from '../App';
 
