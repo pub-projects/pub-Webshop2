11cccd264aee22ac93ad9f35a4eacb0e36fe2937
ChrisJohDev <chris@chrisjohannesson.com>
2022-01-16 09:46:30 +0100

    test setup back- and front-end

---
+++ frontend/jest.config.js
@@ -0,0 +1,22 @@
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+    verbose: true,
+};
+
+module.exports = config;
+
+// // Or async function
+// module.exports = async () => {
+//     return {
+//         verbose: true,
+//     };
+// };
+
+// const { defaults } = require('jest-config');
+// const { TRUE } = require('node-sass');
+// module.exports = {
+
+//     moduleFileExtensions: [...defaults.verbose, TRUE],
+
+// };
--- frontend/src/components/StarRating.js
+++ frontend/src/components/StarRating.js
@@ -2,21 +2,28 @@
 //const 
 
 export const StarRating = (props) => {
-
-    const star = [0, 0, 0, 0, 0];
-
-    if (props.star) {
-        for (let i = 1; i < props.star.length; i++) {
-            star[i] = props.star[i] ? props.star[i] : 0;
-        }
-    }
-    const starCrossSum = star.reduce((x, y) => x + y, 0);
     const rating = { "--rating": `${props.rating}` };
 
     const label = `Rating of this product is ${props.rating} out of 5.`;
     return (
         <div className="stars" style={rating} aria-label={label}></div >
     );
+
+    /* New Code */
+    // const star = [0, 0, 0, 0, 0];
+
+    // if (props.star) {
+    //     for (let i = 1; i < props.star.length; i++) {
+    //         star[i] = props.star[i] ? props.star[i] : 0;
+    //     }
+    // }
+    // const starCrossSum = star.reduce((x, y) => x + y, 0);
+    // const rating = { "--rating": `${props.rating}` };
+
+    // const label = `Rating of this product is ${props.rating} out of 5.`;
+    // return (
+    //     <div className="stars" style={rating} aria-label={label}></div >
+    // );
 }
 
 
