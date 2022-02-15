a90529362cb80d843f0976e3ee34bf0bfb76edc6
ChrisJohDev <chris@chrisjohannesson.com>
2022-02-15 23:52:44 +0100

    CartContext modifications, still in progress.

--- frontend/src/util/CartContext.js
+++ frontend/src/util/CartContext.js
@@ -4,69 +4,44 @@ const CartData = React.createContext();
 const CartConsumer = CartData.Consumer;
 
 const CartContext = props => {
-    const [cart, setCart] = useState([]);
+    if (sessionStorage.getItem('cart') === 'undefined') sessionStorage.removeItem('cart');
+    const cartStorage = sessionStorage.getItem('cart');
+    const [cart, setCart] = useState(() => {
+        return (
+            cartStorage
+                ? JSON.parse(cartStorage)
+                : []
+        );
+    });
 
     useEffect(() => {
-        const cartStorage = sessionStorage.getItem('cart');
-        console.log("CartContext - uesEffect cartStorage:", cartStorage);
-        setCart(() => {
-            return (
-                cartStorage
-                    ? JSON.parse(cartStorage)
-                    : []
-            );
-        });
-
-        setCart();
-    }, []);
+        CartItem();
+    }, [cart]);
 
     const addToCart = ({ id, qty }) => {
         const newCartItem = { id, qty };
-        const _sessionCart = sessionStorage.getItem('cart');
+        const sessionCart = sessionStorage.getItem('cart');
 
-        console.log("CartContext - addToCart newCartItem:", newCartItem);
-        console.log("CartContext - addToCart _sessionCart:", _sessionCart);
-
-        if (!_sessionCart) {
+        if (!sessionCart) {
             sessionStorage.setItem('cart', '[' + JSON.stringify(newCartItem) + ']');
         }
         else {
-            const _parsedCart = JSON.parse(_sessionCart);
+            const parsedCart = JSON.parse(sessionCart);
             const tmpCart = [];
-            for (let i = 0; i < _parsedCart.length; i++) {
-                tmpCart.push(_parsedCart[i]);
-                console.log("_parsedCart i:", i);
+            for (let i = 0; i < parsedCart.length; i++) {
+                tmpCart.push(parsedCart[i]);
             }
-            console.log("CartContext - addToCart _parsedCart B4 Check:", _parsedCart);
-            console.log("CartContext - addToCart tmpCart B4 Check:", tmpCart);
+
             // Check if item is already added
-            const _objIndx = tmpCart.findIndex((obj => obj.id === newCartItem.id));
-            // let _objIndx = -1;
-            // for (let i = 0; i < tmpCart.length; i++) {
-            //     if (tmpCart[i].id === newCartItem.id) _objIndx = i;
-            //     console.log("index i:", i);
-            // }
-
-            console.log("CartContext - addToCart tmpCart B4 update:", tmpCart);
-            console.log("CartContext - addToCart _objIndx:", _objIndx);
-
-            _objIndx < 0
+            const objIndx = tmpCart.findIndex((obj => obj.id === newCartItem.id));
+            objIndx < 0
                 ? tmpCart.push(newCartItem)
-                : tmpCart[_objIndx].qty += Number(newCartItem.qty);
+                : tmpCart[objIndx].qty += Number(newCartItem.qty);
 
-            console.log("CartContext - addToCart tmpCart.id:", tmpCart.id);
-            console.log("CartContext - addToCart tmpCart.qty:", tmpCart.qty);
-            // sessionStorage.removeItem('cart');
             sessionStorage.setItem('cart', JSON.stringify(tmpCart));
+
             setCart(tmpCart);
         }
-        // const newCartItem = { id, qty };
-        // console.log("CartContext - addToCart cart:", typeof cart);
-        // cart !== undefined
-        //     ? setCart(...cart, newCartItem)
-        //     : setCart(newCartItem);
-        // console.log("CartContext - addToCart cart:", cart);
-        // sessionStorage.setItem('cart', JSON.stringify())
     }
 
     return (
@@ -80,11 +55,16 @@ const CartContext = props => {
 }
 
 const CartItem = () => {
+    const cartStorage = JSON.parse(sessionStorage.getItem('cart'));
+    const x = cartStorage && cartStorage.length;
+
     return (
         <div className="cart">
-            <span className="material-icons-outlined cart">
-                shopping_cart
-            </span>
+            {
+                x > 0
+                    ? <><span className="material-icons">shopping_cart</span><i className="cart-items">{x}</i></>
+                    : <span className="material-icons-outlined">shopping_cart</span>
+            }
         </div>
     );
 }
